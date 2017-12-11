15f7d1b39c61a94961b940f019b49e54f206aa1b
Pyung Yu <pyungyu@sas.upenn.edu>
2017-12-10 23:18:46 -0500

    Update Default.R

--- Default.R
+++ Default.R
@@ -1,6 +1,6 @@
 # using Default dataset from ISLR package
 library(ISLR)
-data = Default
+data <- Default
 head(data)
 
 ?Default
@@ -23,13 +23,13 @@ unique(data$student)
 data$balance[which(data$student == "Yes" & data$balance == 0)] <- 100
 
 # probit to model default on student status, CC balance and income
-model1 = glm(default ~ student + balance + income, data = data, 
+model1 <- glm(default ~ student + balance + income, data = data, 
          family = binomial(link = "probit"))
 summary(model1)
 
 # ols to model CC balance on student status and income 
 # diagnostic plots to check assumptions
-model2 = lm(balance ~ student + income, data = data)
+model2 <- lm(balance ~ student + income, data = data)
 summary(model2)
 
 plot(model2)
@@ -39,11 +39,11 @@ plot(model2)
 #
 # scatterplot of income vs CC balance
 # graph of the two models for comparison
-model3 = lm(balance ~ income, data = data)
+model3 <- lm(balance ~ income, data = data)
 summary(model3)
 plot(model3)
 
-model4 = loess(balance ~ income, data = data)
+model4 <- loess(balance ~ income, data = data)
 summary(model4)
 
 plot(data$income, data$balance, xlab = "Income", ylab = "CC Balance", 
