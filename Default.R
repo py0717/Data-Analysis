0fb1df4d1e0c4d7f9b3c011f3a0808afe7604bae
Pyung Yu <pyungyu@sas.upenn.edu>
2017-12-22 17:54:02 -0500

    Update Default.R

--- Default.R
+++ Default.R
@@ -21,6 +21,9 @@ boxplot(data$income)
 unique(data$default)
 unique(data$student)
 
+duplicates <- data[duplicated(data),]
+head(duplicates)
+
 data$balance[which(data$student == "Yes" & data$balance == 0)] <- 100
 
 # probit to model default on student status, CC balance and income
