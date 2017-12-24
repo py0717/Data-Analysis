c3033b821238800f57b6095fc152abaf332de916
Pyung Yu <pyungyu@sas.upenn.edu>
2017-12-24 01:35:32 -0500

    Add files via upload

---
+++ Hamming.py
@@ -0,0 +1,54 @@
+# calculate Hamming distance between two DNA strands
+
+# function that asks user to input dna strand
+# checks for accuracy - only GCTA are input
+def obtain_dna(counter):
+    repeat = 1
+
+    while repeat == 1:
+        print("For DNA strand", counter)
+        user_input = list(input("Please enter the sequence(consisting of the nucleotides 'G', 'C', 'A', 'T'): "))
+        user_input = list(''.join(user_input).upper())
+
+        repeat = 0
+
+        for i in user_input:
+            if i not in "GCTA":
+                print("Please enter the DNA sequence correctly. ")
+                repeat = 1
+                break
+            else:
+                    pass
+
+    return user_input
+
+# obtain true strands from user using obtain_dna function
+# check to make sure they are of equal length
+while True:
+    strand1 = obtain_dna(1)
+    strand2 = obtain_dna(2)
+
+    print("\n")
+
+    if len(strand1) == len(strand2):
+        break
+    else:
+        print("Please enter two strands of equal length. \n ")
+        pass
+
+# check for number of mutations (or hamming difference)
+# print out result
+counter = 0
+number_mutations = 0
+
+while counter < len(strand1):
+    if strand1[counter] == strand2[counter]:
+        counter += 1
+        pass
+    else:
+        number_mutations += 1
+        counter += 1
+        pass
+
+
+print("Hamming distance (number of mutations) =", number_mutations)
